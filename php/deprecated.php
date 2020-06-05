3c2794c643764cc86df47bee32c39fe994faa09a
Ryan Kienstra <kienstraryan@gmail.com>
2020-06-05 13:50:32 -0500

    Migrate block_lab_* funtions, use nampespace for new functions
    Put the namespaced funtions in a new file,
    block-api.php, instead of helpers.php,
    as it looks like best practice is to not mix
    namespaced functions in a file with non-namespaced functions.

--- php/deprecated.php
+++ php/deprecated.php
@@ -11,6 +11,9 @@
  * @license http://opensource.org/licenses/GPL-2.0 GNU General Public License, version 2 (GPL-2.0)
  */
 
+use function Genesis\CustomBlocks\add_block;
+use function Genesis\CustomBlocks\add_field;
+
 /**
  * Show a PHP error to warn developers using deprecated functions.
  *
@@ -18,7 +21,7 @@
  * @param string $version     The version of Genesis Custom Blocks that deprecated the function.
  * @param string $replacement The function that should have been called.
  */
-function block_lab_deprecated_function( $function, $version, $replacement ) {
+function genesis_custom_blocks_deprecated_function( $function, $version, $replacement ) {
 	_deprecated_function(
 		// filter_var is used for sanitization here as it allows arrow functions ("->").
 		filter_var(
@@ -42,7 +45,7 @@ function block_lab_deprecated_function( $function, $version, $replacement ) {
  * @return array
  */
 function block_lab_get_icons() {
-	block_lab_deprecated_function( 'block_lab_get_icons', '1.3.5', 'genesis_custom_blocks()->get_icons()' );
+	genesis_custom_blocks_deprecated_function( 'block_lab_get_icons', '1.3.5', 'genesis_custom_blocks()->get_icons()' );
 	return genesis_custom_blocks()->get_icons();
 }
 
@@ -54,6 +57,55 @@ function block_lab_get_icons() {
  * @return array
  */
 function block_lab_allowed_svg_tags() {
-	block_lab_deprecated_function( 'block_lab_allowed_svg_tags', '1.3.5', 'genesis_custom_blocks()->allowed_svg_tags()' );
+	genesis_custom_blocks_deprecated_function( 'block_lab_allowed_svg_tags', '1.3.5', 'genesis_custom_blocks()->allowed_svg_tags()' );
 	return genesis_custom_blocks()->allowed_svg_tags();
+}
+
+/**
+ * Handle deprecated block_lab_add_block function.
+ *
+ * @param string $block_name   The block name (slug), like 'example-block'.
+ * @param array  $block_config {
+ *     An associative array containing the block configuration.
+ *
+ *     @type string   $title    The block title.
+ *     @type string   $icon     The block icon. See assets/icons.json for a JSON array of all possible values. Default: 'block_lab'.
+ *     @type string   $category The slug of a registered category. Categories include: common, formatting, layout, widgets, embed. Default: 'common'.
+ *     @type array    $excluded Exclude the block in these post types. Default: [].
+ *     @type string[] $keywords An array of up to three keywords. Default: [].
+ *     @type array    $fields {
+ *         An associative array containing block fields. Each key in the array should be the field slug.
+ *
+ *         @type array {$slug} {
+ *             An associative array describing a field. Refer to the $field_config parameter of block_lab_add_field().
+ *         }
+ *     }
+ * }
+ */
+function block_lab_add_block( $block_name, $block_config = [] ) {
+	genesis_custom_blocks_deprecated_function( 'block_lab_add_block', '1.5.4', 'Genesis\\CustomBlocks\\add_block()' );
+	add_block( $block_name, $block_config );
+}
+
+/**
+ * Handle deprecated block_lab_add_field function.
+ *
+ * @param string $block_name The block name (slug), like 'example-block'.
+ * @param string $field_name The field name (slug), like 'first-name'.
+ * @param array  $field_config {
+ *     An associative array containing the field configuration.
+ *
+ *     @type string $name    The field name.
+ *     @type string $label   The field label.
+ *     @type string $control The field control type. Default: 'text'.
+ *     @type int    $order   The order that the field appears in. Default: 0.
+ *     @type array  $settings {
+ *         An associative array of settings for the field. Each field has a different set of possible settings.
+ *         Check the register_settings method for the field, found in php/blocks/controls/class-{field name}.php.
+ *     }
+ * }
+ */
+function block_lab_add_field( $block_name, $field_name, $field_config = [] ) {
+	genesis_custom_blocks_deprecated_function( 'block_lab_add_field', '1.5.4', 'Genesis\\CustomBlocks\\add_field()' );
+	add_field( $block_name, $field_name, $field_config );
 }
