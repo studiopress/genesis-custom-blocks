1375adb81b50c042f5ef84a7196c3a3f253754e6
Ryan Kienstra <kienstraryan@gmail.com>
2020-06-17 23:16:06 -0500

    Add a debugging statement

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -33,4 +33,5 @@ function _manually_load_plugin() {
 tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
 
 // Start up the WP testing environment.
+echo 'About to bootstrap the tests';
 require $_tests_dir . '/includes/bootstrap.php';
