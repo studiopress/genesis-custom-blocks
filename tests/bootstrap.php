34150ad58847c72a02e13dd4262704dae4056e20
Ryan Kienstra <kienstraryan@gmail.com>
2020-06-18 13:59:38 -0500

    Use some debugging statments from a tutorial
    https://www.wpbeginner.com/wp-tutorials/how-to-fix-the-error-establishing-a-database-connection-in-wordpress/

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -34,3 +34,10 @@ tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
 
 // Start up the WP testing environment.
 require $_tests_dir . '/includes/bootstrap.php';
+
+$db_link = mysqli_connect( '127.0.0.1', 'yourusernamehere', 'yourpasswordhere' );
+if ( ! $db_link ) {
+	die( 'Could not connect: ' . mysqli_error() );
+}
+echo 'Connected fine';
+mysqli_close();
