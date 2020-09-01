b0da132e0f71e801c85aee10da99eafaa39df906
Ryan Kienstra <kienstraryan@gmail.com>
2020-09-01 14:38:02 -0500

    Remove the copy about migrating
    We should only see this notice
    if users have BL 1.5.6 instead of 1.6.0

--- php/Plugin.php
+++ php/Plugin.php
@@ -90,7 +90,7 @@ class Plugin extends PluginAbstract {
 	}
 
 	/**
-	 * Require the API functions.
+	 * Requires the API functions.
 	 */
 	private function require_api() {
 		require_once __DIR__ . '/BlockApi.php';
@@ -150,7 +150,7 @@ class Plugin extends PluginAbstract {
 		</style>
 		<div id="gcb-conflict-notice" class="notice notice-error gcb-notice-conflict">
 			<div class="gcb-conflict-copy">
-				<p><?php esc_html_e( 'It looks like Block Lab is active. Please deactivate it or migrate, as it will not work while Genesis Custom Blocks is active.', 'genesis-custom-blocks' ); ?></p>
+				<p><?php esc_html_e( 'It looks like Block Lab is active. Please deactivate it, as it will not work while Genesis Custom Blocks is active.', 'genesis-custom-blocks' ); ?></p>
 			</div>
 			<?php
 			if ( current_user_can( 'deactivate_plugins' ) ) :
