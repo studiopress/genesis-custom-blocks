b675d4a5e424199627809dd8254996e2a2c06d7d
Ryan Kienstra <kienstraryan@gmail.com>
2020-10-22 15:13:32 -0500

    Simplify enqueue_assets()
    Make sure the editor
    continues to work
    without this middleware.

--- php/Admin/EditBlock.php
+++ php/Admin/EditBlock.php
@@ -64,7 +64,6 @@ class EditBlock extends ComponentAbstract {
 	 * as the native editor is disabled.
 	 */
 	public function enqueue_assets() {
-		global $post, $post_type, $post_type_object;
 		$screen = get_current_screen();
 
 		if (
@@ -74,57 +73,6 @@ class EditBlock extends ComponentAbstract {
 		) {
 			return;
 		}
-
-		$rest_base = ! empty( $post_type_object->rest_base ) ? $post_type_object->rest_base : $post_type_object->name;
-
-		// Preload common data.
-		$preload_paths = [
-			'/',
-			'/wp/v2/types?context=edit',
-			'/wp/v2/taxonomies?per_page=-1&context=edit',
-			'/wp/v2/themes?status=active',
-			sprintf( '/wp/v2/%s/%s?context=edit', $rest_base, $post->ID ),
-			sprintf( '/wp/v2/types/%s?context=edit', $post_type ),
-			sprintf( '/wp/v2/users/me?post_type=%s&context=edit', $post_type ),
-			[ '/wp/v2/media', 'OPTIONS' ],
-			[ '/wp/v2/blocks', 'OPTIONS' ],
-			sprintf( '/wp/v2/%s/%d/autosaves?context=edit', $rest_base, $post->ID ),
-		];
-
-		/**
-		 * Preload common data by specifying an array of REST API paths that will be preloaded.
-		 *
-		 * Filters the array of paths that will be preloaded.
-		 *
-		 * @since 5.0.0
-		 *
-		 * @param string[] $preload_paths Array of paths to preload.
-		 * @param WP_Post  $post          Post being edited.
-		 */
-		$preload_paths = apply_filters( 'block_editor_preload_paths', $preload_paths, $post );
-
-		/*
-		 * Ensure the global $post remains the same after API data is preloaded.
-		 * Because API preloading can call the_content and other filters, plugins
-		 * can unexpectedly modify $post.
-		 */
-		$backup_global_post = $post;
-
-		$preload_data = array_reduce(
-			$preload_paths,
-			'rest_preload_api_request',
-			[]
-		);
-
-		// Restore the global $post as it was before API preloading.
-		$post = $backup_global_post; // phpcs:ignore WordPress.WP.GlobalVariablesOverride.Prohibited
-
-		wp_default_packages_inline_scripts( wp_scripts() );
-		wp_add_inline_script(
-			'wp-api-fetch',
-			sprintf( 'wp.apiFetch.use( wp.apiFetch.createPreloadingMiddleware( %s ) );', wp_json_encode( $preload_data ) ),
-			'after'
-		);
 
 		$script_slug   = 'genesis-custom-blocks-edit-block-script';
 		$script_handle = require $this->plugin->get_path( 'js/dist/edit-block.asset.php' );
@@ -141,7 +89,7 @@ class EditBlock extends ComponentAbstract {
 			'gcbEditor',
 			[
 				'postType'     => get_post_type(),
-				'postId'       => isset( $post->ID ) ? $post->ID : null,
+				'postId'       => get_the_ID(),
 				'settings'     => [],
 				'initialEdits' => $this->get_initial_edits(),
 			]
