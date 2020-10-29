5244d15a30a65e1467b8efa4e4d3f9c3535b9d13
Ryan Kienstra <kienstraryan@gmail.com>
2020-10-29 16:37:12 -0600

    Add the <NumberNonNegative> component
    For the maxlength setting,
    like in the Text field.

---
+++ js/src/edit-block/components/settings/number-non-negative.js
@@ -0,0 +1,29 @@
+// @ts-check
+
+/**
+ * External dependencies
+ */
+import React from 'react';
+
+/**
+ * Internal dependencies
+ */
+import { Input } from '../';
+
+/**
+ * @typedef {Object} NumberNonNegativeProps The component props.
+ * @property {Object} setting This setting.
+ * @property {string|undefined} value The setting value.
+ */
+
+/**
+ * The field settings.
+ *
+ * @param {NumberNonNegativeProps} props The component props.
+ * @return {React.ReactElement} The component for the admin page.
+ */
+const NumberNonNegative = ( props ) => {
+	return <Input { ...props } type="number" min={ 0 } />;
+};
+
+export default NumberNonNegative;
