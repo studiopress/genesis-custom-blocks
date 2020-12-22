d103578fb4524cf1629a083ebb1bd15bf1c9ef62
Ryan Kienstra <kienstraryan@gmail.com>
2020-12-22 12:54:34 -0600

    Remove <TinyMCE>, as it wasn't used
    This was defined,
    but never used in this plugin.

--- js/src/block-editor/components/index.js
+++ js/src/block-editor/components/index.js
@@ -3,4 +3,3 @@ export { default as Edit } from './edit';
 export { default as Fields } from './fields';
 export { default as Image } from './image';
 export { default as RepeaterRows } from './repeater-rows';
-export { default as TinyMCE } from './tiny-mce';
