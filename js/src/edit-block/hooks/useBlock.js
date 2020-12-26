90cbb1419902f59c0986f07919aa974569bc5a86
Ryan Kienstra <kienstraryan@gmail.com>
2020-12-26 16:26:59 -0600

    Remove needless useCallback() in useBlock.js
    The dependency arrays had fullBlock,
    which is an object.
    So this will always invalidate
    the function, because React
    uses shallow comparison,
    like Object.is().

--- js/src/edit-block/hooks/useBlock.js
+++ js/src/edit-block/hooks/useBlock.js
@@ -2,7 +2,6 @@
  * WordPress dependencies
  */
 import { useDispatch, useSelect } from '@wordpress/data';
-import { useCallback, useMemo } from '@wordpress/element';
 
 /**
  * Internal dependencies
@@ -51,103 +50,83 @@ const useBlock = () => {
 	);
 	const { editPost } = useDispatch( 'core/editor' );
 
-	const fullBlock = useMemo(
-		() => getBlock( editedPostContent ),
-		[ editedPostContent ]
-	);
+	const fullBlock = getBlock( editedPostContent );
+	const blockNameWithNameSpace = getBlockNameWithNameSpace( fullBlock );
+	const block = fullBlock[ blockNameWithNameSpace ];
 
-	const blockNameWithNameSpace = useMemo(
-		() => getBlockNameWithNameSpace( fullBlock ),
-		[ fullBlock ]
-	);
-
-	const block = useMemo(
-		() => fullBlock[ blockNameWithNameSpace ] || {},
-		[ fullBlock, blockNameWithNameSpace ]
-	);
-
-	const changeBlock = useCallback(
-		/**
-		 * Changes a block's values.
-		 *
-		 * Does not overwrite the whole block, only the values
-		 * passed in newValues.
-		 *
-		 * @param {Object} newValues The new value(s) to set.
-		 */
-		( newValues ) => {
-			const blockName = getBlockNameWithNameSpace( fullBlock );
-			const editedPost = {
-				content: JSON.stringify(
-					{
-						[ blockName ]: {
-							...fullBlock[ blockName ],
-							...newValues,
-						},
-					}
-				),
-			};
-
-			if ( newValues.hasOwnProperty( 'title' ) ) {
-				editedPost.title = newValues.title;
-			}
-
-			editPost( editedPost );
-		},
-		[ editPost, fullBlock ]
-	);
-
-	const changeBlockName = useCallback(
-		/**
-		 * Changes a block name (slug).
-		 *
-		 * @param {string} newName The new bock name (slug).
-		 * @param {Object} [defaultValues] The new block values, if any.
-		 */
-		( newName, defaultValues = {} ) => {
-			const previousBlockName = getBlockNameWithNameSpace( fullBlock );
-			const editedPost = {
-				content: JSON.stringify( {
-					[ `${ BLOCK_NAMESPACE }/${ newName }` ]: {
-						...fullBlock[ previousBlockName ],
-						...defaultValues,
-						name: newName,
+	/**
+	 * Changes a block's values.
+	 *
+	 * Does not overwrite the whole block, only the values
+	 * passed in newValues.
+	 *
+	 * @param {Object} newValues The new value(s) to set.
+	 */
+	const changeBlock = ( newValues ) => {
+		const blockName = getBlockNameWithNameSpace( fullBlock );
+		const editedPost = {
+			content: JSON.stringify(
+				{
+					[ blockName ]: {
+						...fullBlock[ blockName ],
+						...newValues,
 					},
-				} ),
-				name: newName,
-			};
+				}
+			),
+		};
 
-			if ( defaultValues.hasOwnProperty( 'title' ) ) {
-				editedPost.title = defaultValues.title;
-			}
+		if ( newValues.hasOwnProperty( 'title' ) ) {
+			editedPost.title = newValues.title;
+		}
 
-			editPost( editedPost );
-		},
-		[ editPost, fullBlock ]
-	);
+		editPost( editedPost );
+	};
 
-	const setDefaults = useCallback(
-		/**
-		 * If a block property doesn't exist, add a default.
-		 *
-		 * @param {number} postId The current post ID.
-		 */
-		( postId ) => {
-			const defaultBlock = getDefaultBlock( postId );
-			const blockName = block.name ? block.name : defaultBlock.name;
+	/**
+	 * Changes a block name (slug).
+	 *
+	 * @param {string} newName The new bock name (slug).
+	 * @param {Object} [defaultValues] The new block values, if any.
+	 */
+	const changeBlockName = ( newName, defaultValues = {} ) => {
+		const previousBlockName = getBlockNameWithNameSpace( fullBlock );
+		const editedPost = {
+			content: JSON.stringify( {
+				[ `${ BLOCK_NAMESPACE }/${ newName }` ]: {
+					...fullBlock[ previousBlockName ],
+					...defaultValues,
+					name: newName,
+				},
+			} ),
+			name: newName,
+		};
 
-			editPost( {
-				content: JSON.stringify( {
-					[ `${ BLOCK_NAMESPACE }/${ blockName }` ]: {
-						...defaultBlock,
-						...block,
-					},
-				} ),
-				name: blockName,
-			} );
-		},
-		[ block, editPost ]
-	);
+		if ( defaultValues.hasOwnProperty( 'title' ) ) {
+			editedPost.title = defaultValues.title;
+		}
+
+		editPost( editedPost );
+	};
+
+	/**
+	 * If a block property doesn't exist, add a default.
+	 *
+	 * @param {number} postId The current post ID.
+	 */
+	const setDefaults = ( postId ) => {
+		const defaultBlock = getDefaultBlock( postId );
+		const blockName = block.name ? block.name : defaultBlock.name;
+
+		editPost( {
+			content: JSON.stringify( {
+				[ `${ BLOCK_NAMESPACE }/${ blockName }` ]: {
+					...defaultBlock,
+					...block,
+				},
+			} ),
+			name: blockName,
+		} );
+	};
 
 	return {
 		block,
