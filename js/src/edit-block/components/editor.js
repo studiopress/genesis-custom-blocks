ba2ddaed7f799312f69c88b51b01ccc522f1796d
Ryan Kienstra <kienstraryan@gmail.com>
2021-04-22 16:13:57 -0500

    Add a little explanation of how the brackets work
    This probably needs more work.

--- js/src/edit-block/components/editor.js
+++ js/src/edit-block/components/editor.js
@@ -16,6 +16,7 @@ import {
 	UnsavedChangesWarning,
 } from '@wordpress/editor';
 import { StrictMode, useState } from '@wordpress/element';
+import { __, sprintf } from '@wordpress/i18n';
 import ServerSideRender from '@wordpress/server-side-render';
 
 /**
@@ -113,6 +114,8 @@ const Editor = ( { onError, postId, postType, settings } ) => {
 	const [ selectedField, setSelectedField ] = useState( NO_FIELD_SELECTED );
 	const [ templateMode, setTemplateMode ] = useState( MARKUP_TEMPLATE_MODE );
 
+	const urlTemplateDocumentation = 'https://developer.wpengine.com/genesis-custom-blocks/get-started/add-a-custom-block-to-your-website-content/';
+
 	/** @param {Object} newAttributes Attribute (field) name and value. */
 	const setAttributes = ( newAttributes ) => {
 		changeBlock( {
@@ -190,6 +193,29 @@ const Editor = ( { onError, postId, postType, settings } ) => {
 												templateMode={ templateMode }
 												setTemplateMode={ setTemplateMode }
 											/>
+											{
+												MARKUP_TEMPLATE_MODE === templateMode
+													? (
+														<>
+															<span className="block text-sm mt-1 mb-2">
+																{ __( 'To render a field, enter the field name (slug) enclosed in 2 brackets', 'genesis-custom-blocks' ) }
+															</span>
+															<span className="block text-sm mt-1 mb-2">
+																{
+																	sprintf(
+																		/* translators: %1$s: the field name (slug). */
+																		__( 'For example, the field example-text would be %1$s', 'genesis-custom-blocks' ),
+																		'{{example-text}}'
+																	)
+																}
+															</span>
+															<a href={ urlTemplateDocumentation } className="block text-sm mt-1 mb-5">
+																{ __( 'Learn more', 'genesis-custom-blocks' ) }
+															</a>
+														</>
+													)
+													: null
+											}
 											<AceEditor
 												value={ MARKUP_TEMPLATE_MODE === templateMode ? templateMarkup : templateCss }
 												mode={ MARKUP_TEMPLATE_MODE === templateMode ? 'html' : 'css' }
