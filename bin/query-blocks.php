6a69cf753f5cf38adb5c576f01290d98bcd7f0ac
Ryan Kienstra <kienstraryan@gmail.com>
2021-05-24 17:48:19 -0500

    Add a simple WP-CLI file to query for the blocks and get their counts
    This displays them in a table
    with WP-CLI.

---
+++ bin/query-blocks.php
@@ -0,0 +1,64 @@
+<?php
+/**
+ * Queries for the blocks on the site.
+ *
+ * phpcs:ignoreFile
+ * @package Genesis\CustomBlocks
+ */
+
+namespace Genesis\CustomBlocks\Cli;
+
+use WP_CLI;
+use WP_Query;
+use Genesis\CustomBlocks\Blocks\Block;
+
+/**
+ * Gets the field types (controls) and their counts.
+ *
+ * @return array[] An array of associative arrays, with keys of 'field' and 'count'.
+ */
+function get_fields() {
+	$field_histogram = [];
+	$block_query     = new WP_Query( [ 'post_type' =>'genesis_custom_block' ] );
+	foreach( $block_query->posts as $post ) {
+		$block = new Block( $post->ID );
+		foreach( $block->fields as $field ) {
+			$field_histogram[ $field->control ] = ! empty( $field_histogram[ $field->control ] ) && is_int( $field_histogram[ $field->control ] )
+				? $field_histogram[ $field->control ] + 1
+				: 1;
+		}
+	}
+
+	$table_fields = [];
+	foreach ( $field_histogram as $field => $count ) {
+		$table_fields[] = compact( 'field', 'count' );
+	}
+
+	return $table_fields;
+}
+
+/**
+ * Queries the blocks on the site and logs the results.
+ */
+function query_blocks() {
+	$post_count = wp_count_posts( 'genesis_custom_block' );
+	WP_CLI::log( sprintf( 'There are %1$d GCB blocks', $post_count->publish ) );
+	WP_CLI::log( "\n" );
+
+	$table_values = get_fields();
+	$table_fields = [ 'field', 'count' ];
+	$assoc_args   = [
+		'fields' => implode( ',', $table_fields ),
+		'format' => 'table',
+	];
+
+	$formatter = new WP_CLI\Formatter( $assoc_args, $table_fields );
+	$formatter->display_items( $table_values );
+}
+
+if ( ! defined( 'WP_CLI' ) ) {
+ 	echo "Please run this with WP-CLI: wp eval-file bin/query-blocks.php\n";
+	exit( 1 );
+}
+
+query_blocks();
