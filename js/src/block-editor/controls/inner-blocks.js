d1eb49e066a09aa7ead61573a46aa8aeb4ad37c2
Ryan Kienstra <kienstraryan@gmail.com>
2021-06-22 16:01:06 -0500

    Add the label and help for the field

--- js/src/block-editor/controls/inner-blocks.js
+++ js/src/block-editor/controls/inner-blocks.js
@@ -6,8 +6,17 @@ import * as React from 'react';
 /**
  * WordPress dependencies
  */
+import { BaseControl } from '@wordpress/components';
+
+/**
+ * WordPress dependencies
+ */
 import { InnerBlocks } from '@wordpress/block-editor';
 
-const GcbInnerBlocksControl = () => <InnerBlocks />;
+const GcbInnerBlocksControl = ( { field } ) => (
+	<BaseControl label={ field.label } help={ field.help } id={ `gcb-inner-blocks-${ field.name }` }>
+		<InnerBlocks />
+	</BaseControl>
+);
 
 export default GcbInnerBlocksControl;
