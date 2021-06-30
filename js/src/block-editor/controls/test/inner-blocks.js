f3f10e6760787d1392babc7a7fd2fe70b1cf9289
Ryan Kienstra <kienstraryan@gmail.com>
2021-06-30 00:30:24 -0500

    Add a Jest test for the InnerBlocks control

---
+++ js/src/block-editor/controls/test/inner-blocks.js
@@ -0,0 +1,31 @@
+/**
+ * External dependencies
+ */
+import { render, screen } from '@testing-library/react';
+
+/**
+ * Internal dependencies
+ */
+import GcbInnerBlocksControl from '../inner-blocks';
+
+/**
+ * Gets the props for the component.
+ *
+ * @return {Object} The props.
+ */
+const getProps = () => ( {
+	field: {
+		label: 'Here is a label for InnerBlocks',
+		help: 'This is some help text',
+	},
+	onChange: jest.fn(),
+	context: 'edit-block',
+} );
+
+test( 'inner-blocks control', () => {
+	const props = getProps();
+	render( <GcbInnerBlocksControl { ...props } /> );
+	screen.getByText( props.field.help );
+	screen.getByText( props.field.label );
+	screen.getByText( 'This field only works in the block editor.' );
+} );
