97a42be6a43c3d471626518a9799de3ae64b2465
Ryan Kienstra <kienstraryan@gmail.com>
2021-08-02 23:44:44 -0500

    Update e2e tests for WP 5.8
    To make sure thy work there.
    Update e2e-test-utils package.

--- tests/e2e/specs/no-field.js
+++ tests/e2e/specs/no-field.js
@@ -23,7 +23,7 @@ describe( 'NoField', () => {
 		const $editBlockDocument = await getDocument( page );
 		await ( await findByLabelText( $editBlockDocument, /category/i ) ).select( 'media' );
 		( await findByText( $editBlockDocument, /publish/i ) ).click();
-		await findAllByText( $editBlockDocument, /published/i );
+		await findByText( $editBlockDocument, /update/i );
 
 		// Create a new post and add the new block.
 		await createNewPost();
