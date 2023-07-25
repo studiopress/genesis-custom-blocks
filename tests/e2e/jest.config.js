fe8ee76e4ca8e368b0aa94cab504c49d7f561af7
Ryan Kienstra <kienstraryan@gmail.com>
2023-07-25 11:14:38 -0600

    Fix the transform

--- tests/e2e/jest.config.js
+++ tests/e2e/jest.config.js
@@ -1,14 +1,11 @@
 module.exports = {
 	...require( '@wordpress/scripts/config/jest-e2e.config' ),
-	transform: {
-		'^.+\\.[jt]sx?$': '<rootDir>/node_modules/@wordpress/scripts/config/babel-transform',
-	},
 	setupFilesAfterEnv: [
 		'<rootDir>/config/setup-test-framework.js',
 		'@wordpress/jest-console',
 		'expect-puppeteer',
 	],
-	testPathIgnorePatterns: [
-		'/node_modules/',
-	],
+	transform: {
+		'\\.[jt]sx?$': '<rootDir>/../../node_modules/@wordpress/scripts/config/babel-transform',
+	},
 };
