8fdfd542f28dc3f3f558e010deabc8996ae4e971
Ryan Kienstra <kienstraryan@gmail.com>
2023-07-27 11:36:38 -0600

    Remove husky, replace with vanilla git command

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -14,7 +14,7 @@ if ( empty( $_tests_dir ) ) {
 }
 
 if ( ! file_exists( $_tests_dir . '/includes/functions.php' ) ) {
-	echo 'Could not find $_tests_dir/includes/functions.php, have you run bin/install-wp-tests.sh ?' . PHP_EOL; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
+	echo 'Could not find $_tests_dir/includes/functions.php, have you run tests/install-wp-tests.sh ?' . PHP_EOL; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
 	exit( 1 );
 }
 
